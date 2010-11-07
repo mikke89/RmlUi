cfa0f8c58624339fe8a0f26c1ae742c257a6acd8
Hilton Medeiros <medeiros.hilton AT gmail DOT com>
2010-11-07 19:25:21 -0200

    Added some paths variables to files list generator.

--- Build/cmake/gen_filelists.sh
+++ Build/cmake/gen_filelists.sh
@@ -6,36 +6,39 @@ src='set(lib_SRC_FILES'
 hdr='set(lib_HDR_FILES'
 pubhdr='set(lib_PUB_HDR_FILES'
 srcdir='${PROJECT_SOURCE_DIR}'
+srcpath=Source
+hdrpath=Include/Rocket
+pypath=Python
 
 printfiles() {
     # Print headers
     echo ${hdr/lib/$1} >>$file
-    find  Source/$1/ -maxdepth 1 -iname "*.h" -exec echo '    '$srcdir/{} \; >>$file
+    find  $srcpath/$1/ -maxdepth 1 -iname "*.h" -exec echo '    '$srcdir/{} \; >>$file
     echo -e ')\n' >>$file
     # Print public headers
     echo ${pubhdr/lib/$1} >>$file
-    find  Include/Rocket/$1/ -maxdepth 1 -iname "*.h" -exec echo '    '$srcdir/{} \; >>$file
+    find  $hdrpath/$1/ -maxdepth 1 -iname "*.h" -exec echo '    '$srcdir/{} \; >>$file
     # Print main public header
     echo '    '$srcdir/Include/Rocket/$1.h >>$file
     echo -e ')\n' >>$file
     # Print source files
     echo ${src/lib/$1} >>$file
-    find  Source/$1/ -maxdepth 1 -iname "*.cpp" -exec echo '    '$srcdir/{} \; >>$file
+    find  $srcpath/$1/ -maxdepth 1 -iname "*.cpp" -exec echo '    '$srcdir/{} \; >>$file
     echo -e ')\n' >>$file
 }
 
 printpyfiles() {
     # Print headers
     echo ${hdr/lib/Py${1,}} >>$file
-    find  Source/$1/Python -iname "*.h" -exec echo '    '$srcdir/{} \; >>$file
+    find  $srcpath/$1/$pypath -iname "*.h" -exec echo '    '$srcdir/{} \; >>$file
     echo -e ')\n' >>$file
     # Print public headers
     echo ${pubhdr/lib/Py${1,}} >>$file
-    find  Include/Rocket/$1/Python -iname "*.h" -exec echo '    '$srcdir/{} \; >>$file 2>/dev/null
+    find  $hdrpath/$1/$pypath -iname "*.h" -exec echo '    '$srcdir/{} \; >>$file 2>/dev/null
     echo -e ')\n' >>$file
     # Print source files
     echo ${src/lib/Py${1,}} >>$file
-    find  Source/$1/Python -iname "*.cpp" -exec echo '    '$srcdir/{} \; >>$file
+    find  $srcpath/$1/$pypath -iname "*.cpp" -exec echo '    '$srcdir/{} \; >>$file
     echo -e ')\n' >>$file
 }
 
