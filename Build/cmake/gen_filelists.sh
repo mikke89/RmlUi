940514a0a11b45496f5fd874c5a04992dd7939bb
Hilton Medeiros <medeiros.hilton AT gmail DOT com>
2010-11-07 19:23:38 -0200

    Added python bindings build system for cmake.

--- Build/cmake/gen_filelists.sh
+++ Build/cmake/gen_filelists.sh
@@ -8,15 +8,34 @@ pubhdr='set(lib_PUB_HDR_FILES'
 srcdir='${PROJECT_SOURCE_DIR}'
 
 printfiles() {
+    # Print headers
     echo ${hdr/lib/$1} >>$file
     find  Source/$1/ -maxdepth 1 -iname "*.h" -exec echo '    '$srcdir/{} \; >>$file
     echo -e ')\n' >>$file
+    # Print public headers
     echo ${pubhdr/lib/$1} >>$file
     find  Include/Rocket/$1/ -maxdepth 1 -iname "*.h" -exec echo '    '$srcdir/{} \; >>$file
+    # Print main public header
     echo '    '$srcdir/Include/Rocket/$1.h >>$file
     echo -e ')\n' >>$file
+    # Print source files
     echo ${src/lib/$1} >>$file
     find  Source/$1/ -maxdepth 1 -iname "*.cpp" -exec echo '    '$srcdir/{} \; >>$file
+    echo -e ')\n' >>$file
+}
+
+printpyfiles() {
+    # Print headers
+    echo ${hdr/lib/Py${1,}} >>$file
+    find  Source/$1/Python -iname "*.h" -exec echo '    '$srcdir/{} \; >>$file
+    echo -e ')\n' >>$file
+    # Print public headers
+    echo ${pubhdr/lib/Py${1,}} >>$file
+    find  Include/Rocket/$1/Python -iname "*.h" -exec echo '    '$srcdir/{} \; >>$file 2>/dev/null
+    echo -e ')\n' >>$file
+    # Print source files
+    echo ${src/lib/Py${1,}} >>$file
+    find  Source/$1/Python -iname "*.cpp" -exec echo '    '$srcdir/{} \; >>$file
     echo -e ')\n' >>$file
 }
 
@@ -24,6 +43,10 @@ pushd $basedir
 echo -e "# This file was auto-generated with gen_filelists.sh\n" >$file
 for lib in "Core" "Controls" "Debugger"; do
     printfiles $lib
+done
+
+for lib in "Core" "Controls"; do
+    printpyfiles $lib
 done
 popd
 
