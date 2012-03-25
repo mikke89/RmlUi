c29b71f52a011094bf8eea7d0ef87a1fe846f019
Nate Starkey <gambini.nate@gmail.com>
2012-03-25 16:46:12 -0400

    Added an Event type for Lua

---
+++ Source/Core/Lua/Event.h
@@ -0,0 +1,47 @@
+#pragma once
+/*
+    This defines an Event type name in the global Lua namespace
+
+    Generally, you won't create an event yourself, just receive it.
+
+    //method
+    noreturn Event:StopPropagation()
+    
+    //getters
+    Element Event.current_element
+    string Event.type
+    Element Event.target_element
+    {}key=string,value=int,float,Colourb/f,string,Vector2f,userdata Event.parameters
+*/
+
+#include "LuaType.h"
+#include "lua.hpp"
+
+namespace Rocket {
+namespace Core {
+namespace Lua {
+
+//method
+int EventStopPropagation(lua_State* L, Event* obj);
+
+//getters
+int EventGetAttrcurrent_element(lua_State* L);
+int EventGetAttrtype(lua_State* L);
+int EventGetAttrtarget_element(lua_State* L);
+int EventGetAttrparameters(lua_State* L);
+
+//setters
+//int EventSetAttrparameters(lua_State* L);
+
+RegType<Event> EventMethods[];
+luaL_reg EventGetters[];
+luaL_reg EventSetters[];
+
+template<> const char* GetTClassName<Event>();
+template<> RegType<Event>* GetMethodTable<Event>();
+template<> luaL_reg* GetAttrTable<Event>();
+template<> luaL_reg* SetAttrTable<Event>();
+
+}
+}
+}
