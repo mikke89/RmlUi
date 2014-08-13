33f51460019768c72af0017de253b7e5466e9197
David Wimsey <david@wimsey.us>
2014-08-12 22:36:07 -0400

    Minor fix to copyright notice

--- Samples/basic/directx/src/ShellRenderInterfaceExtensionsDirectX_Win32.cpp
+++ Samples/basic/directx/src/ShellRenderInterfaceExtensionsDirectX_Win32.cpp
@@ -3,7 +3,7 @@
  *
  * For the latest information, see http://www.librocket.com
  *
- * Copyright (c) 2014 CodePoint David Wimsey
+ * Copyright (c) 2014 David Wimsey
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
