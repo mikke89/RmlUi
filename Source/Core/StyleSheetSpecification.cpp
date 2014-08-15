409e96bbd788866e67a5a8468c6e48af535c65a0
Aleksey Kuleshov <rndfax@yandex.ru>
2014-08-15 22:28:41 +0400

    satisfy CSS2 specification for LEFT, RIGHT, TOP, BOTTOM

--- Source/Core/StyleSheetSpecification.cpp
+++ Source/Core/StyleSheetSpecification.cpp
@@ -188,16 +188,16 @@ void StyleSheetSpecification::RegisterDefaultProperties()
 
 	RegisterProperty(DISPLAY, "inline", false, true).AddParser("keyword", "none, block, inline, inline-block");
 	RegisterProperty(POSITION, "static", false, true).AddParser("keyword", "static, relative, absolute, fixed");
-	RegisterProperty(TOP, "0px", false, false)
+	RegisterProperty(TOP, "auto", false, false)
 		.AddParser("keyword", "auto")
 		.AddParser("number");
-	RegisterProperty(RIGHT, "0px", false, false).AddParser("number")
+	RegisterProperty(RIGHT, "auto", false, false)
 		.AddParser("keyword", "auto")
 		.AddParser("number");
-	RegisterProperty(BOTTOM, "0px", false, false).AddParser("number")
+	RegisterProperty(BOTTOM, "auto", false, false)
 		.AddParser("keyword", "auto")
 		.AddParser("number");
-	RegisterProperty(LEFT, "0px", false, false).AddParser("number")
+	RegisterProperty(LEFT, "auto", false, false)
 		.AddParser("keyword", "auto")
 		.AddParser("number");
 
