7890511914034bef7c62a8fa34964a89238b3a77
David Wimsey <david@wimsey.us>
2014-09-24 09:05:16 -0400

    Fixed initial call to glOrtho to use the window size rather than hardcoded value

--- Samples/shell/src/win32/ShellRenderInterfaceExtensionsOpenGL_Win32.cpp
+++ Samples/shell/src/win32/ShellRenderInterfaceExtensionsOpenGL_Win32.cpp
@@ -118,7 +118,9 @@ bool ShellRenderInterfaceOpenGL::AttachToNative(void *nativeWindow)
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(0, 1024, 768, 0, -1, 1);
+	RECT crect;
+	GetClientRect(this->window_handle, &crect)
+	glOrtho(0, (crect.right - crect.left), (crect.bottom - crect.top), 0, -1, 1);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
