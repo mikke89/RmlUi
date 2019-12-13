384b03b03cc8e157d998babdaf379de769cd81c8
Michael Ragazzon <michael.ragazzon@gmail.com>
2019-12-13 06:07:12 +0100

    Try fixing problems from the None macro in X11 (ugh)

--- Samples/shell/include/x11/InputX11.h
+++ Samples/shell/include/x11/InputX11.h
@@ -30,6 +30,13 @@
 #define INPUTX11_H
 
 #include <X11/Xlib.h>
+
+// The None define from X.h conflicts with RmlUi code base,
+// use the constant 0L instead where necessary
+#ifdef None
+  #undef None
+#endif
+
 #include "Input.h"
 
 /**
