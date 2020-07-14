97b004070b22cb24962ebff0f2cf55dabd2259ef
Michael Ragazzon <michael.ragazzon@gmail.com>
2020-07-14 15:00:42 +0200

    Add FamilyId to std::hash.

--- Include/RmlUi/Core/Types.h
+++ Include/RmlUi/Core/Types.h
@@ -101,6 +101,7 @@ struct TransitionList;
 struct Rectangle;
 enum class EventId : uint16_t;
 enum class PropertyId : uint8_t;
+enum class FamilyId : int;
 
 // Types for external interfaces.
 using FileHandle = uintptr_t;
@@ -205,6 +206,10 @@ template <> struct hash<::Rml::PropertyId> {
 template <> struct hash<::Rml::Character> {
     using utype = typename ::std::underlying_type<::Rml::Character>::type;
     size_t operator() (const ::Rml::Character& t) const { ::std::hash<utype> h; return h(static_cast<utype>(t)); }
+};
+template <> struct hash<::Rml::FamilyId> {
+	using utype = typename ::std::underlying_type<::Rml::FamilyId>::type;
+	size_t operator() (const ::Rml::FamilyId& t) const { ::std::hash<utype> h; return h(static_cast<utype>(t)); }
 };
 }
 
