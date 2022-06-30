mikke89/RmlUi

commit 2b12e0ad832117b47777d9cfce633f779df3e52f

Don't load default glyphs for fallback fonts

Source/Core/FontEngineDefault/FontFamily.cpp
@@ -56,7 +56,7 @@ FontFaceHandleDefault* FontFamily::GetFaceHandle(Style::FontStyle style, Style::
 	if (matching_face == nullptr)
 		return nullptr;
 
-	return matching_face->GetHandle(size);
+	return matching_face->GetHandle(size, false);
 }
 
 

Source/Core/FontEngineDefault/FontProvider.cpp
@@ -97,7 +97,7 @@ FontFaceHandleDefault* FontProvider::GetFallbackFontFace(int index, int font_siz
 	auto& faces = FontProvider::Get().fallback_font_faces;
 
 	if (index >= 0 && index < (int)faces.size())
-		return faces[index]->GetHandle(font_size);
+		return faces[index]->GetHandle(font_size, true);
 
 	return nullptr;
 }

Source/Core/FontEngineDefault/FontFace.cpp
@@ -65,7 +65,8 @@ Style::FontWeight FontFace::GetWeight() const
 	return weight;
 }
 
-FontFaceHandleDefault* FontFace::GetHandle(int size) {
+FontFaceHandleDefault* FontFace::GetHandle(int size, bool load_default_glyphs)
+{
 	auto it = handles.find(size);
 	if (it != handles.end())
 		return it->second.get();
@@ -79,7 +80,7 @@ FontFaceHandleDefault* FontFace::GetHandle(int size) {
 
 	// Construct and initialise the new handle.
 	auto handle = MakeUnique<FontFaceHandleDefault>();
-	if (!handle->Initialize(face, size))
+	if (!handle->Initialize(face, size, load_default_glyphs))
 	{
 		handles[size] = nullptr;
 		return nullptr;

Source/Core/FontEngineDefault/FontFace.h
@@ -51,8 +51,9 @@ public:
 
 	/// Returns a handle for positioning and rendering this face at the given size.
 	/// @param[in] size The size of the desired handle, in points.
+	/// @param[in] load_default_glyphs True to load the default set of glyph (ASCII range).
 	/// @return The font handle.
-	FontFaceHandleDefault* GetHandle(int size);
+	FontFaceHandleDefault* GetHandle(int size, bool load_default_glyphs);
 
 private:
 	Style::FontStyle style;

Source/Core/FontEngineDefault/FontFaceHandleDefault.cpp
@@ -52,16 +52,14 @@ FontFaceHandleDefault::~FontFaceHandleDefault()
 	layers.clear();
 }
 
-bool FontFaceHandleDefault::Initialize(FontFaceHandleFreetype face, int font_size)
+bool FontFaceHandleDefault::Initialize(FontFaceHandleFreetype face, int font_size, bool load_default_glyphs)
 {
 	ft_face = face;
 
 	RMLUI_ASSERTMSG(layer_configurations.empty(), "Initialize must only be called once.");
 
-	if (!FreeType::InitialiseFaceHandle(ft_face, font_size, glyphs, metrics))
-	{
+	if (!FreeType::InitialiseFaceHandle(ft_face, font_size, glyphs, metrics, load_default_glyphs))
 		return false;
-	}
 
 	has_kerning = FreeType::HasKerning(ft_face);
 	FillKerningPairCache();

Source/Core/FontEngineDefault/FontFaceHandleDefault.h
@@ -51,7 +51,7 @@ public:
 	FontFaceHandleDefault();
 	~FontFaceHandleDefault();
 
-	bool Initialize(FontFaceHandleFreetype face, int font_size);
+	bool Initialize(FontFaceHandleFreetype face, int font_size, bool load_default_glyphs);
 
 	/// Returns the point size of this font face.
 	int GetSize() const;

Source/Core/FontEngineDefault/FreeTypeInterface.cpp
@@ -39,7 +39,7 @@ namespace Rml {
 static FT_Library ft_library = nullptr;
 
 static bool BuildGlyph(FT_Face ft_face, Character character, FontGlyphMap& glyphs, float bitmap_scaling_factor);
-static void BuildGlyphMap(FT_Face ft_face, int size, FontGlyphMap& glyphs, float bitmap_scaling_factor);
+static void BuildGlyphMap(FT_Face ft_face, int size, FontGlyphMap& glyphs, float bitmap_scaling_factor, bool load_default_glyphs);
 static void GenerateMetrics(FT_Face ft_face, FontMetrics& metrics, float bitmap_scaling_factor);
 static bool SetFontSize(FT_Face ft_face, int font_size, float& out_bitmap_scaling_factor);
 static void BitmapDownscale(byte* bitmap_new, int new_width, int new_height, const byte* bitmap_source, int width, int height, int pitch,
@@ -115,7 +115,7 @@ void FreeType::GetFaceStyle(FontFaceHandleFreetype in_face, String& font_family,
 }
 
 // Initialises the handle so it is able to render text.
-bool FreeType::InitialiseFaceHandle(FontFaceHandleFreetype face, int font_size, FontGlyphMap& glyphs, FontMetrics& metrics)
+bool FreeType::InitialiseFaceHandle(FontFaceHandleFreetype face, int font_size, FontGlyphMap& glyphs, FontMetrics& metrics, bool load_default_glyphs)
 {
 	FT_Face ft_face = (FT_Face)face;
 
@@ -126,7 +126,7 @@ bool FreeType::InitialiseFaceHandle(FontFaceHandleFreetype face, int font_size, 
 		return false;
 
 	// Construct the initial list of glyphs.
-	BuildGlyphMap(ft_face, font_size, glyphs, bitmap_scaling_factor);
+	BuildGlyphMap(ft_face, font_size, glyphs, bitmap_scaling_factor, load_default_glyphs);
 
 	// Generate the metrics for the handle.
 	GenerateMetrics(ft_face, metrics, bitmap_scaling_factor);
@@ -194,16 +194,19 @@ bool FreeType::HasKerning(FontFaceHandleFreetype face)
 
 
 
-static void BuildGlyphMap(FT_Face ft_face, int size, FontGlyphMap& glyphs, const float bitmap_scaling_factor)
+static void BuildGlyphMap(FT_Face ft_face, int size, FontGlyphMap& glyphs, const float bitmap_scaling_factor, const bool load_default_glyphs)
 {
-	glyphs.reserve(128);
+	if (load_default_glyphs)
+	{
+		glyphs.reserve(128);
 
-	// Add the ASCII characters now. Other characters are added later as needed.
-	FT_ULong code_min = 32;
-	FT_ULong code_max = 126;
+		// Add the ASCII characters now. Other characters are added later as needed.
+		FT_ULong code_min = 32;
+		FT_ULong code_max = 126;
 
-	for (FT_ULong character_code = code_min; character_code <= code_max; ++character_code)
-		BuildGlyph(ft_face, (Character)character_code, glyphs, bitmap_scaling_factor);
+		for (FT_ULong character_code = code_min; character_code <= code_max; ++character_code)
+			BuildGlyph(ft_face, (Character)character_code, glyphs, bitmap_scaling_factor);
+	}
 
 	// Add a replacement character for rendering unknown characters.
 	Character replacement_character = Character::Replacement;

Source/Core/FontEngineDefault/FreeTypeInterface.h
@@ -50,7 +50,7 @@ bool ReleaseFace(FontFaceHandleFreetype face);
 void GetFaceStyle(FontFaceHandleFreetype face, String& font_family, Style::FontStyle& style, Style::FontWeight& weight);
 
 // Initializes a face for a given font size. Glyphs are filled with the ASCII subset, and the font face metrics are set.
-bool InitialiseFaceHandle(FontFaceHandleFreetype face, int font_size, FontGlyphMap& glyphs, FontMetrics& metrics);
+bool InitialiseFaceHandle(FontFaceHandleFreetype face, int font_size, FontGlyphMap& glyphs, FontMetrics& metrics, bool load_default_glyphs);
 
 // Build a new glyph representing the given code point and append to 'glyphs'.
 bool AppendGlyph(FontFaceHandleFreetype face, int font_size, Character character, FontGlyphMap& glyphs);

commit 18f6a1aad87be93fa7eabfe19843218821608600

Font textures are now initially being generated with the ascii range of characters.
This fixes a silly regression in 2b12e0ad832117b47777d9cfce633f779df3e52f. Now the font textures are no longer being regenerated for every new ascii character encountered.

Added a unit test to ensure this behavior in the future.

Source/Core/FontEngineDefault/FontFamily.cpp
@@ -75,7 +75,7 @@ FontFaceHandleDefault* FontFamily::GetFaceHandle(Style::FontStyle style, Style::
 	if (!matching_face)
 		return nullptr;
 
-	return matching_face->GetHandle(size, false);
+	return matching_face->GetHandle(size, true);
 }
 
 // Adds a new face to the family.

Source/Core/FontEngineDefault/FontProvider.cpp
@@ -98,7 +98,7 @@ FontFaceHandleDefault* FontProvider::GetFallbackFontFace(int index, int font_siz
 	auto& faces = FontProvider::Get().fallback_font_faces;
 
 	if (index >= 0 && index < (int)faces.size())
-		return faces[index]->GetHandle(font_size, true);
+		return faces[index]->GetHandle(font_size, false);
 
 	return nullptr;
 }

Tests/Source/Common/TestsShell.cpp
@@ -217,3 +217,12 @@ Rml::String TestsShell::GetRenderStats()
 
 	return result;
 }
+
+TestsRenderInterface* TestsShell::GetTestsRenderInterface()
+{
+#if defined(RMLUI_TESTS_USE_SHELL)
+	return nullptr;
+#else
+	return &shell_render_interface;
+#endif
+}

Tests/Source/Common/TestsShell.h
@@ -31,6 +31,7 @@
 
 #include <RmlUi/Core/Types.h>
 namespace Rml { class RenderInterface; }
+class TestsRenderInterface;
 
 namespace TestsShell {
 
@@ -53,6 +54,9 @@ namespace TestsShell {
 
 	// Stats only available for the dummy renderer.
 	Rml::String GetRenderStats();
+
+	// Returns nullptr if the dummy renderer is not being used.
+	TestsRenderInterface* GetTestsRenderInterface();
 }
 
 #endif

Tests/Source/UnitTests/Core.cpp
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * This source file is part of RmlUi, the HTML/CSS Interface Middleware
  *
  * For the latest information, see http://github.com/mikke89/RmlUi
@@ -26,13 +26,14 @@
  *
  */
 
+#include "../Common/TestsInterface.h"
 #include "../Common/TestsShell.h"
-#include <RmlUi/Core/Core.h>
 #include <RmlUi/Core/Context.h>
+#include <RmlUi/Core/Core.h>
 #include <RmlUi/Core/Element.h>
 #include <RmlUi/Core/ElementDocument.h>
-#include <doctest.h>
 #include <algorithm>
+#include <doctest.h>
 
 using namespace Rml;
 
@@ -121,4 +122,60 @@ TEST_CASE("core.texture_source_list")
 	document->Close();
 
 	TestsShell::ShutdownShell();
+}
+
+TEST_CASE("core.release_textures")
+{
+	TestsRenderInterface* render_interface = TestsShell::GetTestsRenderInterface();
+	// This test only works with the dummy renderer.
+	if (!render_interface)
+		return;
+
+	render_interface->ResetCounters();
+	const auto& counters = render_interface->GetCounters();
+
+	Context* context = TestsShell::GetContext();
+	REQUIRE(context);
+
+	ElementDocument* document = context->LoadDocument("assets/demo.rml");
+	document->Show();
+	TestsShell::RenderLoop();
+
+	Element* element = document->GetElementById("content");
+
+	SUBCASE("ReleaseTextures")
+	{
+		// Release all textures and verify that the render interface received the release call.
+		Rml::ReleaseTextures();
+		CHECK(counters.generate_texture + counters.load_texture == counters.release_texture);
+
+		// By doing a new context Update+Render the textures should be loaded again.
+		TestsShell::RenderLoop();
+		CHECK(counters.generate_texture + counters.load_texture > counters.release_texture);
+	}
+
+	SUBCASE("FontGlyphCache")
+	{
+		const auto counter_generate_before = counters.generate_texture;
+		const auto counter_release_before = counters.release_texture;
+
+		// Verify that ASCII characters are cached during the first use of the font. Then the font texture should not be regenerated when adding ASCII
+		// characters not previously shown.
+		element->SetInnerRML("Abc!%&()");
+		TestsShell::RenderLoop();
+		CHECK(counters.generate_texture == counter_generate_before);
+
+		// However, when we display a non-ASCII character not part of the initial cache, the font texture needs to be regenerated.
+		element->SetInnerRML(reinterpret_cast<const char*>(u8"π"));
+		TestsShell::RenderLoop();
+		CHECK(counters.generate_texture == counter_generate_before + 1);
+		CHECK(counters.release_texture == counter_release_before + 1);
+	}
+
+	document->Close();
+
+	TestsShell::ShutdownShell();
+
+	// Finally, verify that all generated and loaded textures are released during shutdown.
+	CHECK(counters.generate_texture + counters.load_texture == counters.release_texture);
 }