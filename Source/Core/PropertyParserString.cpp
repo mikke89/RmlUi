46b481ed8d5fd8854223f74396d9627079e2c1d1
Dakror <mail@dakror.de>
2022-12-30 17:19:23 +0100

    switch to library utf-8 encode function

--- Source/Core/PropertyParserString.cpp
+++ Source/Core/PropertyParserString.cpp
@@ -15,7 +15,7 @@
  *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,70 +26,46 @@
  *
  */
 
-#include "PropertyParserString.h"
 #include <sstream>
+
+#include "PropertyParserString.h"
 
 namespace Rml {
 
-PropertyParserString::PropertyParserString() {}
-
-PropertyParserString::~PropertyParserString() {}
-
-std::string unicode_to_utf8(int charcode)
+PropertyParserString::PropertyParserString()
 {
-	std::string result;
-	if (charcode < 128)
-	{
-		result.push_back(charcode);
-	}
-	else
-	{
-		int first_bits = 6;
-		const int other_bits = 6;
-		int first_val = 0xC0;
-		int t = 0;
-		while (charcode >= (1 << first_bits))
-		{
-			{
-				t = 128 | (charcode & ((1 << other_bits) - 1));
-				charcode >>= other_bits;
-				first_val |= 1 << (first_bits);
-				first_bits--;
-			}
-			result.insert(0, 1, t);
-		}
-		t = first_val | charcode;
-		result.insert(0, 1, t);
-	}
-	return result;
+}
+
+PropertyParserString::~PropertyParserString()
+{
 }
 
 // Called to parse a RCSS string declaration.
 bool PropertyParserString::ParseValue(Property& property, const String& value, const ParameterMap& RMLUI_UNUSED_PARAMETER(parameters)) const
 {
 	RMLUI_UNUSED(parameters);
-
+	
 	// parse escaped unicode literals according to https://www.w3.org/TR/CSS2/syndata.html#characters
-
+	
 	std::string str = value;
 	std::string::size_type startIdx = 0;
 	do
 	{
 		startIdx = str.find("\\", startIdx);
-		if (startIdx == std::string::npos)
+		if (startIdx == std::string::npos) 
 			break;
-
+		
 		std::string::size_type endIdx = str.find_first_not_of("0123456789abcdefABCDEF", startIdx + 1);
-		if (endIdx == std::string::npos)
+		if (endIdx == std::string::npos) 
 			endIdx = str.length();
-
+		
 		std::string tmpStr = str.substr(startIdx + 1, endIdx - (startIdx + 1));
 		std::istringstream iss(tmpStr);
-
+		
 		uint32_t cp;
 		if (iss >> std::hex >> cp)
 		{
-			std::string utf8 = unicode_to_utf8(cp);
+			std::string utf8 = StringUtilities::ToUTF8(static_cast<Character>(cp));
 			str.replace(startIdx, 1 + tmpStr.length(), utf8);
 			startIdx += utf8.length();
 		}
@@ -97,10 +73,10 @@ bool PropertyParserString::ParseValue(Property& property, const String& value, c
 			startIdx += 1;
 	}
 	while (true);
-
+	
 	property.value = Variant(str);
 	property.unit = Property::STRING;
-
+	
 	return true;
 }
 
