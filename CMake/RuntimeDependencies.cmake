b3256f8c1cb6cc0c9ce2d7421e4e036c6859e107
Michael Ragazzon <michael.ragazzon@gmail.com>
2024-02-18 15:34:40 +0100

    Avoid using separate libraries for subparts of Core
    In particular for Elements, Layout and FontEngineDefault. Defining these as interface libraries cause issues during export. I believe we would have to export them as separate targets, but that doesn't relly make sense from a client standpoint and causes new issues with exported source files and dependencies. It seems to be a lot less troublesome to use a single CMake library and set properties and attach sources to that directly.
    
    Setup runtime output directory from root, and other minor tweaks.

--- CMake/RuntimeDependencies.cmake
+++ CMake/RuntimeDependencies.cmake
@@ -1,9 +1,20 @@
+#[[
+    Change the runtime output directory of all targets declared in this scope. This is particularly helpful on Windows
+    when building the project as a shared library, so that all DLLs are located together with any built executables. The
+    set() call is guarded against pre-definitions in order to respect consumer choice.
+]]
+function(setup_runtime_output_directory)
+    if(NOT DEFINED CMAKE_RUNTIME_OUTPUT_DIRECTORY)
+        set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin" PARENT_SCOPE)
+    endif()
+endfunction()
+
 # RMLUI_CMAKE_MINIMUM_VERSION_RAISE_NOTICE:
 # From CMake 3.21 there is no need for the following function, as we can set the RUNTIME_DEPENDENCY_SET on
 # install(TARGETS) directly.
 #[[
     Output variable to conditionally set up runtime dependency arguments when installing targets.
-    This feature is only available from CMake 3.21, thus,
+    This feature is only available from CMake 3.21, on older version this feature will be disabled.
     Output variable:
         - RMLUI_RUNTIME_DEPENDENCY_SET_ARG: Argument to use for the install(TARGETS) command.
 ]]
