22be063e573ab5c5199b0844f1c6cc705692f99f
Michael Ragazzon <michael.ragazzon@gmail.com>
2024-03-02 23:51:40 +0100

    Implement CMake option `RMLUI_CUSTOM_RTTI`
    Previously: `DISABLE_RTTI_AND_EXCEPTIONS`.
    
    Rename define RMLUI_USE_CUSTOM_RTTI to RMLUI_CUSTOM_RTTI

--- Include/RmlUi/Core/Traits.h
+++ Include/RmlUi/Core/Traits.h
@@ -94,7 +94,7 @@ public:
 
 } // namespace Rml
 
-#ifdef RMLUI_USE_CUSTOM_RTTI
+#ifdef RMLUI_CUSTOM_RTTI
 
 	#define RMLUI_RTTI_Define(_NAME_)                             \
 		using RttiClassType = _NAME_;                             \
@@ -174,6 +174,6 @@ const char* rmlui_type_name()
 	return typeid(T).name();
 }
 
-#endif // RMLUI_USE_CUSTOM_RTTI
+#endif // RMLUI_CUSTOM_RTTI
 
 #endif // RMLUI_CORE_TRAITS_H
