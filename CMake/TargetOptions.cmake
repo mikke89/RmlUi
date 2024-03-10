c8278b6b5b3f675872aa360db93575516580d3dd
Michael Ragazzon <michael.ragazzon@gmail.com>
2024-03-10 19:51:08 +0100

    Add option for recommended compiler flags
    The compiler flags are enabled by default to ensure users get a good experience when building the library without fiddling with options. Especially on MSVC, the default compiler flags means really slow builds (no /MP), and we also risk exposing more warnings.

--- CMake/TargetOptions.cmake
+++ CMake/TargetOptions.cmake
@@ -1,5 +1,5 @@
 #[[
-	Set compile options and features that are common to all RmlUi targets.
+	Set compiler options and features that are common to all RmlUi targets.
 	Arguments:
 		- target: The name of the target to set
 ]]
@@ -7,22 +7,27 @@ function(set_common_target_options target)
 	target_compile_features(${target} PUBLIC cxx_std_14)
 	set_target_properties(${target} PROPERTIES C_EXTENSIONS OFF CXX_EXTENSIONS OFF)
 
-	if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU")
-		target_compile_options(${target} PRIVATE -Wall -Wextra -pedantic)
+	if(RMLUI_WARNINGS_AS_ERRORS AND NOT RMLUI_COMPILER_OPTIONS)
+		message(FATAL_ERROR "Option RMLUI_WARNINGS_AS_ERRORS requires RMLUI_COMPILER_OPTIONS=ON.")
+	endif()
 
-		if(RMLUI_WARNINGS_AS_ERRORS)
-			target_compile_options(${target} PRIVATE -Werror)
-		endif()
-	elseif(MSVC)
-		target_compile_options(${target} PRIVATE /W4 /w44062 /permissive-)
-		target_compile_definitions(${target} PRIVATE _CRT_SECURE_NO_WARNINGS)
+	if(RMLUI_COMPILER_OPTIONS)
+		if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+			target_compile_options(${target} PRIVATE -Wall -Wextra -pedantic)
 
-		if(CMAKE_GENERATOR MATCHES "Visual Studio")
-			target_compile_options(${target} PRIVATE /MP)
-		endif()
+			if(RMLUI_WARNINGS_AS_ERRORS)
+				target_compile_options(${target} PRIVATE -Werror)
+			endif()
+		elseif(MSVC)
+			target_compile_options(${target} PRIVATE /W4 /w44062 /permissive-)
+			target_compile_definitions(${target} PRIVATE _CRT_SECURE_NO_WARNINGS)
 
-		if(RMLUI_WARNINGS_AS_ERRORS)
-			target_compile_options(${target} PRIVATE /WX)
+			if(CMAKE_GENERATOR MATCHES "Visual Studio")
+				target_compile_options(${target} PRIVATE /MP)
+			endif()
+			if(RMLUI_WARNINGS_AS_ERRORS)
+				target_compile_options(${target} PRIVATE /WX)
+			endif()
 		endif()
 	endif()
 endfunction()
