d6161371d7cf3d98d859621e9b694b736836692c
Michael Ragazzon <michael.ragazzon@gmail.com>
2024-03-16 18:10:26 +0100

    Install licenses from CMake to avoid duplicate workflow code

--- CMake/Utils.cmake
+++ CMake/Utils.cmake
@@ -126,3 +126,53 @@ function(install_sample_target target)
 		DESTINATION "${CMAKE_INSTALL_DATADIR}/${sample_path}"
 	)
 endfunction()
+
+#[[
+	Install all license files, including for all installed packages in vcpkg if in use.
+]]
+function(install_licenses)
+	set(bin_licenses_dir "${CMAKE_CURRENT_BINARY_DIR}/Licenses")
+	configure_file("${PROJECT_SOURCE_DIR}/LICENSE.txt"
+		"${bin_licenses_dir}/LICENSE.txt" COPYONLY
+	)
+	configure_file("${PROJECT_SOURCE_DIR}/Include/RmlUi/Core/Containers/LICENSE.txt"
+		"${bin_licenses_dir}/LICENSE.Core.ThirdParty.txt" COPYONLY
+	)
+	configure_file("${PROJECT_SOURCE_DIR}/Source/Debugger/LICENSE.txt"
+		"${bin_licenses_dir}/LICENSE.Debugger.ThirdParty.txt" COPYONLY
+	)
+
+	if(VCPKG_TOOLCHAIN)
+		set(vcpkg_share_dir "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/share")
+		file(GLOB copyright_files "${vcpkg_share_dir}/*/copyright")
+		foreach(copyright_file IN LISTS copyright_files)
+			get_filename_component(name ${copyright_file} DIRECTORY)
+			get_filename_component(name ${name} NAME)
+			if(NOT "${name}" MATCHES "^vcpkg-")
+				set(copy_destination "${bin_licenses_dir}/Dependencies/${name}.txt")
+				configure_file(${copyright_file} ${copy_destination} COPYONLY)
+			endif()
+		endforeach()
+	endif()
+
+	install(DIRECTORY "${bin_licenses_dir}/" DESTINATION "${CMAKE_INSTALL_DATADIR}")
+endfunction()
+
+#[[
+	Install text file with build info.
+]]
+function(install_build_info)
+	if(NOT RMLUI_ARCHITECTURE OR NOT RMLUI_COMMIT_DATE OR NOT RMLUI_RUN_ID OR NOT RMLUI_SHA)
+		message(FATAL_ERROR "Missing variables")
+	endif()
+	generate_rmlui_version_string()
+	file(WRITE "${CMAKE_CURRENT_BINARY_DIR}/Build.txt"
+		"RmlUi ${RMLUI_VERSION_SHORT} binaries for ${RMLUI_ARCHITECTURE}.\n\n"
+		"https://github.com/mikke89/RmlUi\n\n"
+		"Built using ${CMAKE_GENERATOR} (${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION}) on ${RMLUI_COMMIT_DATE} (run ${RMLUI_RUN_ID}).\n"
+		"Commit id: ${RMLUI_SHA}"
+	)
+	install(FILES "${CMAKE_CURRENT_BINARY_DIR}/Build.txt"
+		DESTINATION "${CMAKE_INSTALL_DATADIR}"
+	)
+endfunction()
