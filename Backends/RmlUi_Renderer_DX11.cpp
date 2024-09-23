f22dac3cd6dd18d8a7099847c5267e2b3b8acfbb
Hyblocker <hyblocker@protonmail.com>
2024-09-23 10:30:39 +0200

    (fix): Make input layout declaration more compact

--- Backends/RmlUi_Renderer_DX11.cpp
+++ Backends/RmlUi_Renderer_DX11.cpp
@@ -299,31 +299,11 @@ void RenderInterface_DX11::Init(ID3D11Device* p_d3d_device, ID3D11DeviceContext*
 
     // @TODO:
     {
-        D3D11_INPUT_ELEMENT_DESC polygonLayout[3]{};
-
-        polygonLayout[0].SemanticName = "POSITION";
-        polygonLayout[0].SemanticIndex = 0;
-        polygonLayout[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;
-        polygonLayout[0].InputSlot = 0;
-        polygonLayout[0].AlignedByteOffset = 0;
-        polygonLayout[0].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
-        polygonLayout[0].InstanceDataStepRate = 0;
-
-        polygonLayout[1].SemanticName = "COLOR";
-        polygonLayout[1].SemanticIndex = 0;
-        polygonLayout[1].Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
-        polygonLayout[1].InputSlot = 0;
-        polygonLayout[1].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
-        polygonLayout[1].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
-        polygonLayout[1].InstanceDataStepRate = 0;
-
-        polygonLayout[2].SemanticName = "TEXCOORD";
-        polygonLayout[2].SemanticIndex = 0;
-        polygonLayout[2].Format = DXGI_FORMAT_R32G32_FLOAT;
-        polygonLayout[2].InputSlot = 0;
-        polygonLayout[2].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
-        polygonLayout[2].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
-        polygonLayout[2].InstanceDataStepRate = 0;
+        D3D11_INPUT_ELEMENT_DESC polygonLayout[] = {
+            {"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+            {"COLOR", 0, DXGI_FORMAT_R8G8B8A8_UNORM_SRGB, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+            {"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+        };
 
         // Get a count of the elements in the layout.
         uint32_t numElements = sizeof(polygonLayout) / sizeof(polygonLayout[0]);
