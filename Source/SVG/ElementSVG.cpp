cc008bcad68a4ec19b124124a01dd469fed64742
Michael Ragazzon <michael.ragazzon@gmail.com>
2024-11-02 13:52:40 +0100

    SVG plugin: Check bitmap validity before using it

--- Source/SVG/ElementSVG.cpp
+++ Source/SVG/ElementSVG.cpp
@@ -186,6 +186,8 @@ void ElementSVG::UpdateTexture()
 	auto texture_callback = [this](const CallbackTextureInterface& texture_interface) -> bool {
 		RMLUI_ASSERT(svg_document);
 		lunasvg::Bitmap bitmap = svg_document->renderToBitmap(render_dimensions.x, render_dimensions.y);
+		if (!bitmap.valid() || !bitmap.data())
+			return false;
 
 		// Swap red and blue channels, assuming LunaSVG v2.3.2 or newer, to convert to RmlUi's expected RGBA-ordering.
 		const size_t bitmap_byte_size = bitmap.width() * bitmap.height() * 4;
@@ -193,8 +195,6 @@ void ElementSVG::UpdateTexture()
 		for (size_t i = 0; i < bitmap_byte_size; i += 4)
 			std::swap(bitmap_data[i], bitmap_data[i + 2]);
 
-		if (!bitmap.valid() || !bitmap.data())
-			return false;
 		if (!texture_interface.GenerateTexture({reinterpret_cast<const Rml::byte*>(bitmap.data()), bitmap_byte_size}, render_dimensions))
 			return false;
 		return true;
