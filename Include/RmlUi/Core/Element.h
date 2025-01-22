c3432b3e8f5a509109e8f80b68e6933832d4384b
James Benton <james.benton2@gmail.com>
2025-01-22 23:23:00 +0000

    Change Element::UpdateProperties to be a public API.

--- Include/RmlUi/Core/Element.h
+++ Include/RmlUi/Core/Element.h
@@ -611,12 +611,12 @@ public:
 	/// Return the computed values of the element's properties. These values are updated as appropriate on every Context::Update.
 	const ComputedValues& GetComputedValues() const;
 
+	/// Updates definition, computed values, and runs OnPropertyChange on this element.
+	void UpdateProperties(float dp_ratio, Vector2f vp_dimensions);
+
 protected:
 	void Update(float dp_ratio, Vector2f vp_dimensions);
 	void Render();
-
-	/// Updates definition, computed values, and runs OnPropertyChange on this element.
-	void UpdateProperties(float dp_ratio, Vector2f vp_dimensions);
 
 	/// Forces the element to generate a local stacking context, regardless of the value of its z-index property.
 	void ForceLocalStackingContext();
