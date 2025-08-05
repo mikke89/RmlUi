8f11e0ff7d5da8054fd9bc22bdffcf079aefbbdf
mcukstorm <207544775+mcukstorm@users.noreply.github.com>
2025-08-05 13:45:47 +0100

    Support value attribute on button and submit input elements for HTML conformance (#798)

--- Source/Core/Elements/InputTypeButton.h
+++ Source/Core/Elements/InputTypeButton.h
@@ -30,6 +30,7 @@
 #define RMLUI_CORE_ELEMENTS_INPUTTYPEBUTTON_H
 
 #include "../../../Include/RmlUi/Core/ElementDocument.h"
+#include "../../../Include/RmlUi/Core/ElementText.h"
 #include "../../../Include/RmlUi/Core/EventListener.h"
 #include "InputType.h"
 
@@ -51,6 +52,11 @@ public:
 	/// @return True if the form control is to be submitted, false otherwise.
 	bool IsSubmitted() override;
 
+	/// Called when an attribute of the element has changed.
+	/// @param[in] changed_attributes The attributes that have changed.
+	/// @return True if no layout is required, false if the layout needs to be dirtied.
+	bool OnAttributeChange(const ElementAttributes& changed_attributes) override;
+
 	/// Checks for necessary functional changes in the control as a result of the event.
 	/// @param[in] event The event to process.
 	void ProcessDefaultAction(Event& event) override;
@@ -58,6 +64,9 @@ public:
 	/// Sizes the dimensions to the element's inherent size.
 	/// @return False.
 	bool GetIntrinsicDimensions(Vector2f& dimensions, float& ratio) override;
+
+private:
+	ElementText* value_element = nullptr;
 };
 
 } // namespace Rml
