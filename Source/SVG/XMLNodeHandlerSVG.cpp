a1daa50ef53baf3ca1f7a2c12f59e879f8f6513c
mcukstorm <mcukstorm@users.noreply.github.com>
2025-11-03 00:07:09 +0000

    SVG Plugin inline SVG amendments - change static cast to dynamic cast

--- Source/SVG/XMLNodeHandlerSVG.cpp
+++ Source/SVG/XMLNodeHandlerSVG.cpp
@@ -32,9 +32,10 @@ namespace Rml {
 namespace SVG {
 	bool XMLNodeHandlerSVG::ElementData(XMLParser* parser, const String& data, XMLDataType /*type*/)
 	{
-		auto* element = rmlui_static_cast<ElementSVG*>(parser->GetParseFrame()->element);
+		auto* element = rmlui_dynamic_cast<ElementSVG*>(parser->GetParseFrame()->element);
 		RMLUI_ASSERT(element);
-		element->SetInnerRML(data);
+		if (element)
+			element->SetInnerRML(data);
 		return true;
 	}
 } // namespace SVG
