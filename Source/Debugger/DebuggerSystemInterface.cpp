37f455f6998278b42f4577905ea71d04bfb48e87
Michael Ragazzon <michael.ragazzon@gmail.com>
2026-08-08 00:24:00 +0200

    Add system interface proxy, use in debugger

--- Source/Debugger/DebuggerSystemInterface.cpp
+++ Source/Debugger/DebuggerSystemInterface.cpp
@@ -4,62 +4,14 @@
 namespace Rml {
 namespace Debugger {
 
-DebuggerSystemInterface::DebuggerSystemInterface(Rml::SystemInterface* _application_interface, ElementLog* _log)
-{
-	application_interface = _application_interface;
-	log = _log;
-}
-
-DebuggerSystemInterface::~DebuggerSystemInterface()
-{
-	application_interface = nullptr;
-}
-
-double DebuggerSystemInterface::GetElapsedTime()
-{
-	return application_interface->GetElapsedTime();
-}
-
-int DebuggerSystemInterface::TranslateString(String& translated, const String& input)
-{
-	return application_interface->TranslateString(translated, input);
-}
-
-void DebuggerSystemInterface::JoinPath(String& translated_path, const String& document_path, const String& path)
-{
-	application_interface->JoinPath(translated_path, document_path, path);
-}
+DebuggerSystemInterface::DebuggerSystemInterface(Rml::SystemInterface* application_interface, ElementLog* log) :
+	Rml::SystemInterfaceProxy(application_interface), log(log)
+{}
 
 bool DebuggerSystemInterface::LogMessage(Log::Type type, const String& message)
 {
 	log->AddLogMessage(type, message);
-
-	return application_interface->LogMessage(type, message);
-}
-
-void DebuggerSystemInterface::SetMouseCursor(const String& cursor_name)
-{
-	application_interface->SetMouseCursor(cursor_name);
-}
-
-void DebuggerSystemInterface::SetClipboardText(const String& text)
-{
-	application_interface->SetClipboardText(text);
-}
-
-void DebuggerSystemInterface::GetClipboardText(String& text)
-{
-	application_interface->GetClipboardText(text);
-}
-
-void DebuggerSystemInterface::ActivateKeyboard(Rml::Vector2f caret_position, float line_height)
-{
-	application_interface->ActivateKeyboard(caret_position, line_height);
-}
-
-void DebuggerSystemInterface::DeactivateKeyboard()
-{
-	application_interface->DeactivateKeyboard();
+	return upstream->LogMessage(type, message);
 }
 
 } // namespace Debugger
--- Source/Debugger/DebuggerSystemInterface.h
+++ Source/Debugger/DebuggerSystemInterface.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../../Include/RmlUi/Core/SystemInterface.h"
+#include "../../Include/RmlUi/Core/SystemInterfaceProxy.h"
 
 namespace Rml {
 namespace Debugger {
@@ -12,55 +12,17 @@ class ElementLog;
     application's system interface and passes all the calls through.
  */
 
-class DebuggerSystemInterface : public Rml::SystemInterface {
+class DebuggerSystemInterface : public Rml::SystemInterfaceProxy {
 public:
 	/// Instances a new debugging log interface.
+	/// @param[in] application_interface The upstream system interface to forward calls to.
 	/// @param[in] log The logging element to send messages to.
 	DebuggerSystemInterface(Rml::SystemInterface* application_interface, ElementLog* log);
-	virtual ~DebuggerSystemInterface();
 
-	/// Get the number of seconds elapsed since the start of the application.
-	/// @return Elapsed time, in seconds.
-	double GetElapsedTime() override;
-
-	/// Translate the input string into the translated string.
-	/// @param[out] translated Translated string ready for display.
-	/// @param[in] input String as received from XML.
-	/// @return Number of translations that occured.
-	int TranslateString(String& translated, const String& input) override;
-
-	/// Joins the path of an RML or RCSS file with the path of a resource specified within the file.
-	/// @param[out] translated_path The joined path.
-	/// @param[in] document_path The path of the source document (including the file name).
-	/// @param[in] path The path of the resource specified in the document.
-	void JoinPath(String& translated_path, const String& document_path, const String& path) override;
-
-	/// Log the specified message.
-	/// @param[in] type Type of log message, ERROR, WARNING, etc.
-	/// @param[in] message Message to log.
-	/// @return True to continue execution, false to break into the debugger.
+	/// Intercept log messages.
 	bool LogMessage(Log::Type type, const String& message) override;
 
-	/// Set mouse cursor.
-	/// @param[in] cursor_name Cursor name to activate.
-	void SetMouseCursor(const String& cursor_name) override;
-
-	/// Set clipboard text.
-	/// @param[in] text Text to apply to clipboard.
-	void SetClipboardText(const String& text) override;
-
-	/// Get clipboard text.
-	/// @param[out] text Retrieved text from clipboard.
-	void GetClipboardText(String& text) override;
-
-	/// Activate keyboard (for touchscreen devices).
-	void ActivateKeyboard(Rml::Vector2f caret_position, float line_height) override;
-
-	/// Deactivate keyboard (for touchscreen devices).
-	void DeactivateKeyboard() override;
-
 private:
-	Rml::SystemInterface* application_interface;
 	ElementLog* log;
 };
 
